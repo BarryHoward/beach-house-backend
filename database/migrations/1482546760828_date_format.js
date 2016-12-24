cd182cba0c4f1e555f9fcb0d5c319ebc63417a75
Barry Howard <BarryLeeHoward@gmail.com>
2016-12-23 21:37:31 -0500

    added day,month,year and removed week_of

---
+++ database/migrations/1482546760828_date_format.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class DateFormatTableSchema extends Schema {
+
+  up () {
+    this.table('comments', (table) => {
+      table.dropColumn('week_of')
+      table.integer('day')
+      table.string('month')
+      table.integer('year')
+    })
+  }
+
+  down () {
+    this.table('comments', (table) => {
+      table.date('week_of')
+      table.dropColumn('day')
+      table.dropColumn('month')
+      table.dropColumn('year')
+  }
+
+}
+
+module.exports = DateFormatTableSchema
