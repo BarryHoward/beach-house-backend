1f6f2f46faa9a15d8e951f05584dfc9454133f3f
Barry Howard <BarryLeeHoward@gmail.com>
2017-01-10 04:51:07 -0500

    console logs

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -51,6 +51,7 @@ class UserController {
 		if (user){
 			let people = yield Person.query().table('people')
 				.where("user_id", user.id)
+			console.log(people)
 			user.people = people;
 			response.status(200).json(user)
 		} else {
